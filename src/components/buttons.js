9400329b6377deb80370f124fd799df8e99544ad
exploitenomah <iksploiting@gmail.com>
2022-12-25 23:49:41 +0100

    create button  slide effect

--- src/components/buttons.js
+++ src/components/buttons.js
@@ -2,7 +2,7 @@ import { useCallback, createElement } from "react"
 
 export const ButtonBase = ({ children, onClick, as, ...props}) => {
 
-  const defaultBtnStyles = "whitespace-nowrap cursor-pointer uppercase px-6 py-4"
+  const defaultBtnStyles = "whitespace-nowrap cursor-pointer uppercase px-6 py-4 overflow-hidden"
   const handleClick = useCallback((e) => {
     //post button click
     // e.target.dataset.name
--- src/pages/Buttons.js
+++ src/pages/Buttons.js
@@ -6,12 +6,18 @@ import { ButtonBase } from "../components/buttons";
 export default function Buttons() {
   
   return (
-    <main className="flex">
-      <ButtonBase className={`relative text-purple-800 linear duration-300 border-2 border-purple-800 capitalize overflow-hidden
+    <main className="flex flex-wrap gap-6 items-center align-center justify-start">
+      <ButtonBase className={`relative text-purple-800 linear duration-300 border-2 border-purple-800
         before:absolute before:w-full before:h-full before:-z-10 before:bg-purple-900/80
         before:top-0 before:left-0 before:rotate-[-90deg] before:origin-bottom-left
-        before:linear before:duration-300 before:delay-200 hover:before:rotate-0 hover:text-white hover:rounded-md`}>
+        before:linear before:duration-300 before:delay-75 hover:before:rotate-0 hover:text-white hover:rounded-md`}>
          rotate effect
+      </ButtonBase>
+      <ButtonBase className={`relative text-purple-800 linear duration-300 border-2 border-purple-800
+        before:absolute before:w-full before:h-full before:-z-10 before:bg-purple-900/80
+        before:top-0 before:left-0 before:translate-x-[-100%] before:origin-bottom-left
+        before:linear before:duration-300 before:delay-75 hover:before:translate-x-0 hover:text-white hover:rounded-md`}>
+         slide effect
       </ButtonBase>
     </main>
   )
