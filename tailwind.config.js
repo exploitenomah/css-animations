e460ee679a28921eb10e0bf6b7e95876519a3262
exploitenomah <iksploiting@gmail.com>
2022-12-27 22:59:21 +0100

    create rotate-x utilities

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,17 @@
 /** @type {import('tailwindcss').Config} */
+const plugin = require('tailwindcss/plugin')
+
+// Rotate X utilities
+const rotateX = plugin(function ({ addUtilities }) {
+  addUtilities({
+    '.rotate-x-0': { 
+      transform: 'rotateX(0deg)',
+    },
+    '.rotate-x-270': {
+      transform: 'rotateX(270deg)',
+    },
+  })
+})
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
@@ -6,5 +19,5 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [rotateX],
 }
